3827a5c6a2bae2a8e3fbd25d0eae4f7721366cb7
ToastedFood <76954090+ToastedFood@users.noreply.github.com>
2023-07-21 14:02:38 -0400

    labels and press e

--- food scenes/BananaHitBox.gd
+++ food scenes/BananaHitBox.gd
@@ -4,22 +4,33 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var inreach = false
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_parent().find_node("Panel").visible = false
+	get_parent().find_node("Name").set_text(get_parent().name)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
+func _process(delta):
+	if(inreach == true && Input.is_action_just_pressed("e")):
+			var ScoreText = player.find_node("ScoreText")
+			if(player.find_node("Objective").getCurFruit() == get_parent().name):
+				ScoreText.increaseScore()
+				player.find_node("Objective").newFruit()
 
 func _on_BananaHitBox_body_entered(body):
 	if("Player" in body.name):# Replace with function body.
-		var fruit = body.find_node("Objective").getCurFruit()
-		if(fruit == get_parent().name):
-			var ScoreText = body.find_node("ScoreText")
-			ScoreText.increaseScore()
-			body.find_node("Objective").newFruit()
+		player = body
+		get_parent().find_node("Panel").visible = true
+		inreach = true
+
+func _on_BananaHitBox_body_exited(body):
+	if("Player" in body.name):
+		get_parent().find_node("Panel").visible = false
+		inreach = false # Replace with function body.
