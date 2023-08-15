60f0f884c46187ff41374015a703fb9c9b426731
ToastedFood <76954090+ToastedFood@users.noreply.github.com>
2023-08-15 13:23:41 -0400

    writing an if statement because someone else cant

--- Carts/ShoppingCartSpawn.gd
+++ Carts/ShoppingCartSpawn.gd
@@ -21,9 +21,10 @@ func _on_Timer_timeout():
 	var speed = get_parent().get_parent().get_parent().find_node("Player").getNumFruits()
 	$Timer.wait_time = randf() * (3 - speed/(speed + 10.0)*3) + 1
 	var shoppingCart = ShoppingCart.instance()
-	shoppingCart.setSpeed((0.5 + speed/15.0) * shoppingCart.getSpeed())
-	shoppingCart.set_position(get_position())
-	get_parent().add_child(shoppingCart)
+	if(abs((get_parent().get_parent().get_parent().find_node("Player").position - position).x) >= 50 && abs((get_parent().get_parent().get_parent().find_node("Player").position - position).y) >= 50):
+		shoppingCart.setSpeed((0.5 + speed/15.0) * shoppingCart.getSpeed())
+		shoppingCart.set_position(get_position())
+		get_parent().add_child(shoppingCart)
 	set_position( Vector2(randf() * get_parent().rect_size.x, randf() * get_parent().rect_size.y))
 	#shoppingCart.position = position
 	
